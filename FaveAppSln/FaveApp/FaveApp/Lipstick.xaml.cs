14b82e0de7f8c7b4b62cde7a0f19b446bef3740c
studentsimone <55529034+studentsimone@users.noreply.github.com>
2019-11-05 09:26:50 +0200

    In Process

---
+++ FaveAppSln/FaveApp/FaveApp/Lipstick.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FaveApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page : ContentPage
+    {
+        public Page()
+        {
+            InitializeComponent();
+
+            MakeUp = new List<MakeUp>();
+            MakeUp.Add(new MakeUp
+            {
+                Name = "Raining Men Shade",
+                Price = "ZAR 200",
+                ImageUrl = "https://cdn.shopify.com/s/files/1/0635/0797/products/s1926807-main-zoom_grande_2x.jpg?v=1516416796"
+            });
+
+            BindingContext = this;
+
+        }
+    }
+}
---
+++ FaveAppSln/FaveApp/FaveApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FaveApp
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public IList<MakeUp> MakeUp { get; private set; }
+
+        public  MainPage()
+        {
+            InitializeComponent();
+
+
+           
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Lipstick());
+
+            MakeUp = new List<MakeUp>();
+        }
+    }
+} 
+
